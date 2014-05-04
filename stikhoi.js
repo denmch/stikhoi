e4eda5510380ec9d95741b70a5f9c8c765db2450
Den McHenry <dmmchenry@gmail.com>
2014-05-03 23:38:43 -0700

    Update stikhoi.js
    Improved to account for cases where the start attribute isn't explicit.

--- stikhoi.js
+++ stikhoi.js
@@ -1,11 +1,12 @@
-// For use with stikhoi.css to improve the appearance
-// of line numbers in ancient verse.
+// jQuery. For use with stikhoi.css to improve the
+// appearance of line numbers in classical verse.
 
 $(document).ready(function(){
   $('ol.verse').each(function() {
+    $('ol.verse').not('[start]').attr('start', '1');
     var start = $(this).attr('start');
     $(this).attr(
-      { style: "counter-reset: poem " + (start - 1) }
+      { style: "counter-reset: poem " + (start - 1)  || '1'}
     );
   });
   $("ol.verse").addClass('jQuery');
